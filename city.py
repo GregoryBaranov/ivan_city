08724a4d7cddbde6d1a69b586c064d4f81ce8925
Gregory Baranov <47480142+GregoryBaranov@users.noreply.github.com>
2021-02-28 17:16:43 +0300

    Update city.py

--- city.py
+++ city.py
@@ -1,57 +1,36 @@
-from time import sleep
+from turtle import *
+from random import randint
+tsize = 20
+s_width = 200
+s_height = 180
+class Sprite(Turtle):
+  def __init__(self, x, y, step=10, shape='circle', color='black'):
+      super().__init__()
+      self.penup()
+      self.speed(0)
+      self.goto(x, y)
+      self.color(color)
+      self.shape(shape)
+      self.step = step
+      self.points = 0
  
-class Hero():
-   #конструктор класса
-   def __init__(self, name, health, armor):
-       self.name = name
-       self.health = health #число
-       self.armor = armor #строка
-   #печать параметров персонажа:
-   def print_info(self):
-       print('Уровень здоровья:', self.health)
-       print('Класс брони:', self.armor, '\n')
+  def move_up(self):
+      self.goto(self.xcor(), self.ycor() + self.step)
+  def move_down(self):
+      self.goto(self.xcor(), self.ycor() - self.step)
+  def move_left(self):
+      self.goto(self.xcor() - self.step, self.ycor())
+  def move_right(self):
+      self.goto(self.xcor() + self.step, self.ycor())
  
+player = Sprite(0, -100, 10, 'circle', 'orange')
+enemy1 = Sprite(-s_width, 0, 15, 'square', 'red')
+enemy2 = Sprite(s_width, 70, 15, 'square', 'red')
+goal = Sprite(0, 120, 20, 'triangle', 'green')
  
-class Warrior(Hero):
-   def hello(self):
-       print('-> НОВЫЙ ГЕРОЙ. Верхом на коне появился бравый воин по имени', self.name)
-       self.print_info()
-       sleep(4)
- 
-   def attack(self, enemy):
-       print('-> УДАР! Храбрый воин', self.name, 'атакует', enemy.name, 'мечом!')
-       enemy.armor -= 15 #сила удара для класса Воин
-       if enemy.armor < 0:
-           enemy.health += enemy.armor
-           enemy.armor = 0
-       print('Страшный удар обрушился на противника. \nТеперь его броня: ' +
-           str(enemy.armor) + ', а уровень здоровья: ' + str(enemy.health) + '\n')
-       sleep(5)
- 
-class Magician(Hero):
-   def hello(self):
-       print('-> НОВЫЙ ГЕРОЙ. Откуда ни возьмись появился искусный волшебник', self.name)
-       self.print_info()
-       sleep(4)
- 
-   def attack(self, enemy):
-       print('-> УДАР! Ловкий маг', self.name, 'накладывает заклинание на', enemy.name)
-       enemy.armor -= 35 #сила удара для класса Маг
-       if enemy.armor < 0:
-           enemy.health += enemy.armor
-           enemy.armor = 0
-       print('Сложное заклинание оглушило противника. \nТеперь его броня: ' +
-           str(enemy.armor) + ', а уровень здоровья: ' + str(enemy.health) + '\n')
-       sleep(5)
- 
- 
-Warrior1 = Warrior('Henry', 100, 50)
-Warrior1.hello()
- 
-Magician1 = Magician('Luke', 50, 20)
-Magician1.hello()
- 
-Warrior1.attack(Magician1)
-Magician1.attack(Warrior1)
-
- 
+scr = player.getscreen()
+scr.listen()
+scr.onkey(player.move_up, 'Up')
+scr.onkey(player.move_left, 'Left')
+scr.onkey(player.move_right, 'Right')
+scr.onkey(player.move_down, 'Down')
