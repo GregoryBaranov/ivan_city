0c12cac44661eb529172d9aea40229d31b5db87f
Gregory Baranov <47480142+GregoryBaranov@users.noreply.github.com>
2021-03-18 18:06:32 +0300

    Update city.py

--- city.py
+++ city.py
@@ -1,155 +1,81 @@
-from pygame import *
+from turtle import *
 from random import randint
-# подгружаем отдельно функции для работы со шрифтом
-font.init()
-# во время игры пишем надписи размера 36
-font = font.Font(None, 36)
  
-# нам нужны такие картинки:
-img_win = "thumb.jpg" # фон победы
-img_los = "game-over.jpg" # фон проигрыша
-img_back = "galaxy.jpg" # фон игры
+tsize = 20
+s_width = 200
+s_height = 180
  
-img_bullet = "bullet.png" # пуля
-img_hero = "rocket.png" # герой
-img_enemy = "ufo_1.png" # враг
+class Sprite(Turtle):
+   def __init__(self, x, y, step=10, shape='circle', color='black'):
+       Turtle.__init__(self)
+       self.penup()
+       self.speed(0)
+       self.goto(x, y)
+       self.color(color)
+       self.shape(shape)
+       self.step = step
+       self.points = 0
  
-score = 0 # сбито кораблей
-goal = 10 # столько кораблей нужно сбить для победы
-lost = 0 # пропущено кораблей
-max_lost = 100 # проиграли, если пропустили столько
+   def move_up(self):
+       self.goto(self.xcor(), self.ycor() + self.step)
+   def move_down(self):
+       self.goto(self.xcor(), self.ycor() - self.step)
+   def move_left(self):
+       self.goto(self.xcor() - self.step, self.ycor())
+   def move_right(self):
+       self.goto(self.xcor() + self.step, self.ycor())
  
-# класс-родитель для других спрайтов
-class GameSprite(sprite.Sprite):
-  # конструктор класса
-  def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-      # Вызываем конструктор класса (Sprite):
-      sprite.Sprite.__init__(self)
+   def is_collide(self, sprite):
+       dist = self.distance(sprite.xcor(), sprite.ycor())
+       if dist < 30:
+           return True
+       else:
+           return False
  
-      # каждый спрайт должен хранить свойство image - изображение
-      self.image = transform.scale(image.load(player_image), (size_x, size_y))
-      self.speed = player_speed
+   def set_move(self, x_start, y_start, x_end, y_end):
+       self.x_start = x_start
+       self.y_start = y_start       
+       self.x_end = x_end
+       self.y_end = y_end
+       self.goto(x_start, y_start)
+       self.setheading(self.towards(x_end, y_end)) #направление
+  
+   def make_step(self):
+       self.forward(self.step) #направление уже есть
  
-      # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-      self.rect = self.image.get_rect()
-      self.rect.x = player_x
-      self.rect.y = player_y
+       if self.distance(self.x_end, self.y_end) < self.step: #если расстояние меньше полушага
+           self.set_move(self.x_end, self.y_end, self.x_start, self.y_start) #меняем направление
  
-  # метод, отрисовывающий героя на окне
-  def reset(self):
-      window.blit(self.image, (self.rect.x, self.rect.y))
-# класс главного игрока
-class Player(GameSprite):
-  # метод для управления спрайтом стрелками клавиатуры
-  def update(self):
-      keys = key.get_pressed()
-      if keys[K_LEFT] and self.rect.x > 5:
-          self.rect.x -= self.speed
-      if keys[K_RIGHT] and self.rect.x < win_width - 80:
-          self.rect.x += self.speed
-  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
-  def fire(self):
-      bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-      bullets.add(bullet)
-# класс спрайта-врага   
-class Enemy(GameSprite):
-  # движение врага
-  def update(self):
-      self.rect.y += self.speed
-      global lost
-      # исчезает, если дойдет до края экрана
-      if self.rect.y > win_height:
-          self.rect.x = randint(80, win_width - 80)
-          self.rect.y = 0
-          lost += 1
+player = Sprite(0, -100, 10, 'circle', 'orange')
+enemy1 = Sprite(-s_width, 0, 15, 'square', 'red')
+enemy1.set_move(-s_width, 0, s_width, 0)
+enemy2 = Sprite(s_width, 70, 15, 'square', 'red')
+enemy2.set_move(s_width, 70, -s_width, 70)
+goal = Sprite(0, 120, 20, 'triangle', 'green')
+#goal.set_move(-s_width, 120, s_width, 0)  
  
-# класс спрайта-пули   
-class Bullet(GameSprite):
-  # движение врага
-  def update(self):
-      self.rect.y += self.speed
-      # исчезает, если дойдет до края экрана
-      if self.rect.y < 0:
-          self.kill()
+total_score = 0
  
-# Создаем окошко
-win_width = 700
-win_height = 500
-display.set_caption("Шмап")
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
+scr = player.getscreen()
  
-# создаем спрайты
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 3)
+scr.listen()
  
-# создание группы спрайтов-врагов
-monsters = sprite.Group()
-for i in range(1, 6):
-  monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, 1)
-  monsters.add(monster)
+scr.onkey(player.move_up, 'Up')
+scr.onkey(player.move_left, 'Left')
+scr.onkey(player.move_right, 'Right')
+scr.onkey(player.move_down, 'Down')
  
-bullets = sprite.Group()
+while total_score < 3:
+   enemy1.make_step()
+   enemy2.make_step()
+   #goal.make_step()
+   if player.is_collide(goal):
+       total_score += 1
+       player.goto(0, -100)
+   if player.is_collide(enemy1) or player.is_collide(enemy2):
+       goal.hideturtle()
+       break
  
-# переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
-finish = False
-# Основной цикл игры:
-run = True # флаг сбрасывается кнопкой закрытия окна
-while run:
-    for e in event.get():
-        if e.type == QUIT:
-            run = False
-      # событие нажатия на пробел - спрайт стреляет
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                ship.fire()
- 
-  # сама игра: действия спрайтов, проверка правил игры, перерисовка
-    if not finish:
-        window.blit(background,(0,0))
-    
-        # пишем текст на экране
-        text = font.render("Счет: " + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 20))
-    
-        text_lose = font.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
-    
-        # производим движения спрайтов
-        ship.update()
-        monsters.update()
-        bullets.update()
-    
-        # обновляем их в новом местоположении при каждой итерации цикла
-        ship.reset()
-        monsters.draw(window)
-        bullets.draw(window)
- 
-      # проверка столкновения пули и монстров (и монстр, и пуля при касании исчезают)
-        collides = sprite.groupcollide(monsters, bullets, True, True)
-        for c in collides:
-            # этот цикл повторится столько раз, сколько монстров подбито
-            score = score + 1
-            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, 1)
-            monsters.add(monster)
- 
-      # возможный проигрыш: пропустили слишком много или герой столкнулся с врагом
-        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
-            finish = True # проиграли, ставим фон и больше не управляем спрайтами.
-            # вычисляем отношение
-            img = image.load(img_los)
-            d = img.get_width() // img.get_height()
-            window.fill((255, 255, 255))
-            window.blit(transform.scale(img, (win_height * d, win_height)), (90, 0))
-
-      # проверка выигрыша: сколько очков набрали?
-        if score >= goal:
-            finish = True
-            img = image.load(img_win)
-            window.fill((255, 255, 255))
-            window.blit(transform.scale(img, (win_width, win_height)), (0, 0))
-    
-        display.update()
-  # цикл срабатывает каждую 0.05 секунд
-time.delay(50)
- 
- 
+if total_score == 3:
+   enemy1.hideturtle()
+   enemy2.hideturtle()
